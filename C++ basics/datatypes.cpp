04d734a1758ac8e03b06ad45b8fdaec2a9cb9fbf
Anuj <anprohacks007@gmail.com>
2022-06-16 01:32:43 +0530

    cpp basics - dataypes and Input/output

---
+++ C++ basics/datatypes.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    cout<<"Size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"Size of float "<<sizeof(b)<<endl;
+
+    char c;
+    cout<<"Size of char "<<sizeof(c)<<endl;
+
+    bool d;
+    cout<<"Size of bool "<<sizeof(d)<<endl;
+
+    long int li;
+    short int si;
+
+    cout<<"Size of long int "<<sizeof(li)<<endl;
+
+    cout<<"Size of short int "<<sizeof(si)<<endl;
+    
+    return 0;
+}
